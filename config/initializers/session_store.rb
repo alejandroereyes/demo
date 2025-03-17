728a9e1738cacc7b44f89ca2cf01a74d2f0a05e5
Alejandro Reyes <alejandroereyes@gmail.com>
2025-03-17 12:02:59 -0500

    store session in redis

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+Rails.application.config.session_store :redis_store,
+                                       url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/0/session'),
+                                       expire_after: 90.minutes,
+                                       key: "_#{Rails.application.name.downcase}_session_#{Rails.env}",
+                                       domain: ENV.fetch('DOMAIN_NAME', 'localhost')
