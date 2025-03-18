76e961a62bdb20838cc142b0adf5eca5d8154a14
Alejandro Reyes <alejandroereyes@gmail.com>
2025-03-18 13:39:04 -0500

    add csrf token to rtk headers

--- app/javascript/rootApi.js
+++ app/javascript/rootApi.js
@@ -3,7 +3,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const rootApi = createApi({
     reducerPath: 'user',
     tagTypes: ['CURRENT_USER'],
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3000/api',
+        prepareHeaders: (headers) => {
+            const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+            if (csrfToken) {
+                headers.set('X-CSRF-Token', csrfToken)
+            }
+        }
+    }),
     endpoints: (build) => ({
         fetchCurrentUser: build.query({
             query: () => '/users',
